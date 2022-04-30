9d148d8dadfdc05665d74a55bc954e8de9fd6c24
Pranav Agarwal <79962251+pranav2305@users.noreply.github.com>
2022-04-30 06:31:15 +0000

    Link javascript for bootstrap

--- frontend/wiki_translator/pages/_app.js
+++ frontend/wiki_translator/pages/_app.js
@@ -3,8 +3,14 @@ import "../public/styles/global.css";
 import MainNavbar from "../components/MainNavbar";
 import Head from 'next/head';
 import { CookiesProvider } from "react-cookie";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        import ('bootstrap/dist/js/bootstrap.js')
+    }, [])
+    
   return (
     <CookiesProvider>
       <Head>
